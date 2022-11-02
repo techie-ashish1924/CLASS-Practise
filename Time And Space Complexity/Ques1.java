b98abfe01feb55a24cc004f4ba9c7eaccdd579ff
Ashish Sengar <techie.sengar1924@gmail.com>
2022-11-02 18:17:12 +0530

    final commit

---
+++ Time And Space Complexity/Ques1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ques1 {
+
+    public static void main(String[] args) {
+        
+        long start = System.currentTimeMillis();
+
+        for(int i=0;i<1000000;i++)
+        {
+
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+    
+}
