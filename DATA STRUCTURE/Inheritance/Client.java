238d0d36c9b0556c5651f777e78d7e9d4f160acc
Ashish Sengar <techie.sengar1924@gmail.com>
2022-11-17 14:48:59 +0530

    final commit

--- DATA STRUCTURE/Inheritance/Client.java
+++ DATA STRUCTURE/Inheritance/Client.java
@@ -18,7 +18,7 @@ public class Client {
 */
         // CASE 2
 
-        P obj = new C();
+/*         P obj = new C();
         System.out.println(obj.d);   // 1
         System.out.println(obj.d1);  // 10
 
@@ -30,5 +30,31 @@ public class Client {
         obj.fun1();
 
         ((C) (obj)).fun2();
+
+*/
+
+        // CASE 3
+
+        // C obj = new P();
+        // System.out.println(obj.d);
+        // System.out.println(obj.d1);
+        // System.out.println(obj.d2);
+
+
+        // CASE 4
+
+        C obj = new C();
+        System.out.println(obj.d);   // 2
+        System.out.println(obj.d1);  // 10
+        System.out.println(obj.d2);  // 20
+
+        System.out.println(((P)(obj)).d);
+
+        obj.fun();
+        obj.fun1();
+        obj.fun2();
+
+
+
     }
 }
